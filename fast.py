8bb51b79510dc33a2c8b3c9e9d2ac4f4d3126826
GeorgeCocks-01 <geococks01@gmail.com>
2022-07-11 17:16:42 +0100

    made a fast and slow program to show the Z boson invariant mass peak from the mu mu decay

---
+++ fast.py
@@ -0,0 +1,36 @@
+import ROOT
+import uproot
+import numpy as np
+import matplotlib.pyplot as plt
+
+restMass = 0.105658
+
+
+with uproot.open("/tmp/13TeV__2018__magnet_down_data__Z_candidates.root") as file:
+  mup_PT = file["DecayTree"]["mup_PT"].array(library="np")/1000
+  mup_ETA = file["DecayTree"]["mup_ETA"].array(library="np")
+  mup_PHI = file["DecayTree"]["mup_PHI"].array(library="np")
+  mum_PT = file["DecayTree"]["mum_PT"].array(library="np")/1000
+  mum_ETA = file["DecayTree"]["mum_ETA"].array(library="np")
+  mum_PHI = file["DecayTree"]["mum_PHI"].array(library="np")
+
+mup_theta = 2*np.arctan(np.exp(-mup_ETA))
+mum_theta = 2*np.arctan(np.exp(-mum_ETA))
+
+mup_r = mup_PT/np.sin(mup_theta)
+mum_r = mum_PT/np.sin(mum_theta)
+
+mup_x = mup_r*np.sin(mup_theta)*np.cos(mup_PHI)
+mup_y = mup_r*np.sin(mup_theta)*np.sin(mup_PHI)
+mup_z = mup_r*np.cos(mup_theta)
+mum_x = mum_r*np.sin(mum_theta)*np.cos(mum_PHI)
+mum_y = mum_r*np.sin(mum_theta)*np.sin(mum_PHI)
+mum_z = mum_r*np.cos(mum_theta)
+
+mup_E = np.sqrt(restMass**2 + mup_x**2 + mup_y**2 + mup_z**2)
+mum_E = np.sqrt(restMass**2 + mum_x**2 + mum_y**2 + mum_z**2)
+
+invMass = np.sqrt((mup_E + mum_E)**2 - (mup_x + mum_x)**2 - (mup_y + mum_y)**2 - (mup_z + mum_z)**2)
+
+plt.hist(invMass, bins = 200, range = [0, 200], histtype='step')
+plt.savefig("zpeak_f.png")
---
+++ slow.py
@@ -0,0 +1,44 @@
+import ROOT
+import matplotlib.pyplot as plt
+import numpy as np
+tree = ROOT.TChain("DecayTree")
+
+tree.Add("/tmp/13TeV__2018__magnet_down_data__Z_candidates.root")
+
+restMass = 0.105658
+
+fig = plt.figure()
+
+invMassArray = np.array([])
+
+for entry in tree:
+  mup_PT = entry.mup_PT/1000
+  mup_ETA = entry.mup_ETA
+  mup_PHI = entry.mup_PHI
+
+  mum_PT = entry.mum_PT/1000
+  mum_ETA = entry.mum_ETA
+  mum_PHI = entry.mum_PHI
+
+  mup_theta = 2*np.arctan(np.exp(-mup_ETA))
+  mum_theta = 2*np.arctan(np.exp(-mum_ETA))
+
+  mup_r = mup_PT/np.sin(mup_theta)
+  mum_r = mum_PT/np.sin(mum_theta)
+
+  mup_x = mup_r*np.sin(mup_theta)*np.cos(mup_PHI)
+  mup_y = mup_r*np.sin(mup_theta)*np.sin(mup_PHI)
+  mup_z = mup_r*np.cos(mup_theta)
+  mum_x = mum_r*np.sin(mum_theta)*np.cos(mum_PHI)
+  mum_y = mum_r*np.sin(mum_theta)*np.sin(mum_PHI)
+  mum_z = mum_r*np.cos(mum_theta)
+
+  mup_E = np.sqrt(restMass**2 + mup_x**2 + mup_y**2 + mup_z**2)
+  mum_E = np.sqrt(restMass**2 + mum_x**2 + mum_y**2 + mum_z**2)
+
+  invMassArray = np.append(invMassArray, np.sqrt((mup_E + mum_E)**2 - (mup_x + mum_x)**2 - (mup_y + mum_y)**2 - (mup_z + mum_z)**2))
+
+
+
+plt.hist(invMassArray, bins = 200, range = [0, 200], histtype='step')
+plt.savefig("zpeak_s.png")
