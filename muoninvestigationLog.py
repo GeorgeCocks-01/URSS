17bb322e3a67d6f0c0d68617601c18c48cef34cd
GeorgeCocks-01 <geococks01@gmail.com>
2022-07-20 16:08:00 +0100

    Created muoninvestigationLog.py, which plots log10(isolation) using mostly the same code as in muoninvestigation.py

--- muoninvestigationLog.py
+++ muoninvestigationLog.py
@@ -19,30 +19,33 @@ with uproot.open("/tmp/13TeV_2017_29r2_Up_QcdBgdPt18GeV_Sim09k.root") as file:
   QCDtrueID = file["WpNoMuID/DecayTree/mu_TRUEID"].array(library = "np")
 
 
-#subplots for the isolation each at different small ranges of muon pT (code from compare.py)
+#subplots for log10(isolation) each at different small ranges of muon pT (code from compare.py)
 start = float(input("Enter the start value for small pT range: "))
 
 ipLim = 4
 trLim = 2
+
+logQCD_PTSUMCONE040 = np.log10(np.clip(QCD_PTSUMCONE040, 0.1, None))
+logreal_PTSUMCONE040 = np.log10(np.clip(real_PTSUMCONE040, 0.1, None))
 
 fig, ax = plt.subplots(2, 2)
 fig.suptitle("Isolation comparison at different values of muon pT for ")
 
 for i in range(0,2):
   for j in range(0,2):
-    cropped_real_PTSUMCONE040 = real_PTSUMCONE040[(real_PT > start) & (real_PT < start + 5) & (realIPCHI2 < ipLim) & (realTRCHI2 < trLim) & (realETA < 4.5) & (realETA > 2)]
-    cropped_QCD_PTSUMCONE040 = QCD_PTSUMCONE040[(QCD_PT > start) & (QCD_PT < start + 5) & (simIPCHI2 < ipLim) & (simTRCHI2 < trLim) & (QCDETA < 4.5) & (QCDETA > 2) & (QCDtrueID != 0 )]
+    cropped_real_PTSUMCONE040 = logreal_PTSUMCONE040[(real_PT > start) & (real_PT < start + 5) & (realIPCHI2 < ipLim) & (realTRCHI2 < trLim) & (realETA < 4.5) & (realETA > 2)]
+    cropped_QCD_PTSUMCONE040 = logQCD_PTSUMCONE040[(QCD_PT > start) & (QCD_PT < start + 5) & (simIPCHI2 < ipLim) & (simTRCHI2 < trLim) & (QCDETA < 4.5) & (QCDETA > 2) & (QCDtrueID != 0 )]
 
 
-    ax[i, j].hist(cropped_real_PTSUMCONE040, bins = 50, range = [-2, 140], histtype = 'step', density = True, label = "Real pT")
-    ax[i, j].hist(cropped_QCD_PTSUMCONE040, bins = 50, range = [-2, 140], histtype = 'step', density = True, label = "Simulated QCD pT")
+    ax[i, j].hist(cropped_real_PTSUMCONE040, bins = 50, histtype = 'step', label = "Real pT", density = True)
+    ax[i, j].hist(cropped_QCD_PTSUMCONE040, bins = 50, histtype = 'step', label = "Simulated QCD pT", density = True)
     title = str(start) + " < pT < " + str(start + 5)
     ax[i, j].set_title(title, fontsize = "small")
-    plt.setp(ax[-1, :], xlabel='isolation (GeV)')
+    plt.setp(ax[-1, :], xlabel='log10(isolation (GeV))')
     plt.setp(ax[:, 0], ylabel='Normalised Counts')
 
     start += 5
 
 plt.subplots_adjust(hspace = 0.3, wspace = 0.3)
-plt.legend(fontsize = "x-small")
-plt.savefig("img/muoninvestigation.png")
+plt.legend(fontsize = "x-small", loc = "upper left")
+plt.savefig("img/muoninvestigationLog.png")
