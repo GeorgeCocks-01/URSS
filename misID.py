a456cc5f910a947ef060e70d7abf1b9f9eab3d29
GeorgeCocks-01 <geococks01@gmail.com>
2022-07-22 17:36:56 +0100

    Adapted misIDcheck.py to fit a curve to the data from misID.py, giving length as fit parameter. Fixed true_divide error in misID.py

--- misID.py
+++ misID.py
@@ -9,28 +9,30 @@ with uproot.open("/tmp/13TeV_2017_29r2_Up_QcdBgdPt18GeV_Sim09k.root") as file:
   trueID = file["WpNoMuID/DecayTree/mu_TRUEID"].array(library = "np")
   ETA = file["WpNoMuID/DecayTree/mu_ETA"].array(library = "np")
 
-P = PT*np.cosh(ETA)
+P = PT[PT > 20]*np.cosh(ETA[PT > 20])
+trueID = trueID[PT > 20]
+isMuon = isMuon[PT > 20]
 
-kaons_P = P[(trueID == 321) & (P > 1)]
-pions_P = P[(trueID == 211) & (P > 1)]
-protons_P = P[(trueID == 2212) & (P > 1)]
-type0_P = P[(trueID == 0) & (P > 1)]
+kaons_P = P[(trueID == 321)]
+pions_P = P[(trueID == 211)]
+protons_P = P[(trueID == 2212)]
+type0_P = P[(trueID == 0)]
 
-kaons_misID = P[(isMuon == True) & (trueID == 321) & (P > 1)]
-pions_misID = P[(isMuon == True) & (trueID == 211) & (P > 1)]
-protons_misID = P[(isMuon == True) & (trueID == 2212) & (P > 1)]
-type0_misID = P[(isMuon == True) & (trueID == 0) & (P > 1)]
+kaons_misID = P[(isMuon == True) & (trueID == 321)]
+pions_misID = P[(isMuon == True) & (trueID == 211)]
+protons_misID = P[(isMuon == True) & (trueID == 2212)]
+type0_misID = P[(isMuon == True) & (trueID == 0)]
 
-kaons_hist, kaons_bins = np.histogram(kaons_P, bins = 50, range = [20, 200])
-pions_hist, pions_bins = np.histogram(pions_P, bins = 50, range = [20, 200])
-protons_hist, protons_bins = np.histogram(protons_P, bins = 50, range = [20, 200])
-type0_hist, type0_bins = np.histogram(type0_P, bins = 50, range = [20, 200])
+kaons_hist, kaons_bins = np.histogram(kaons_P, bins = 50, range = [55, 1000])
+pions_hist, pions_bins = np.histogram(pions_P, bins = 50, range = [55, 1000])
+protons_hist, protons_bins = np.histogram(protons_P, bins = 50, range = [55, 1000])
+type0_hist, type0_bins = np.histogram(type0_P, bins = 50, range = [55, 1000])
 
 
-kaons_frac = np.histogram(kaons_misID, bins = 50, range = [20, 200])[0]/kaons_hist
-pions_frac = np.histogram(pions_misID, bins = 50, range = [20, 200])[0]/pions_hist
-protons_frac = np.histogram(protons_misID, bins = 50, range = [20, 200])[0]/protons_hist
-type0_frac = np.histogram(type0_misID, bins = 50, range = [20, 200])[0]/type0_hist
+kaons_frac = np.histogram(kaons_misID, bins = 50, range = [55, 1000])[0]/kaons_hist
+pions_frac = np.histogram(pions_misID, bins = 50, range = [55, 1000])[0]/pions_hist
+protons_frac = np.histogram(protons_misID, bins = 50, range = [55, 1000])[0]/protons_hist
+type0_frac = np.histogram(type0_misID, bins = 50, range = [55, 1000])[0]/type0_hist
 
 
 kaons_err = np.sqrt((kaons_frac*(1 - kaons_frac))/kaons_hist)
@@ -38,13 +40,15 @@ pions_err = np.sqrt((pions_frac*(1 - pions_frac))/pions_hist)
 protons_err = np.sqrt((protons_frac*(1 - protons_frac))/protons_hist)
 type0_err = np.sqrt((type0_frac*(1 - type0_frac))/type0_hist)
 
-
+plt.xscale("log")
 plt.errorbar(x = kaons_bins[:-1], y = kaons_frac*100, label = "Kaons", yerr = kaons_err*100)
 plt.errorbar(x = pions_bins[:-1], y = pions_frac*100, label = "Pions", yerr = pions_err*100)
 plt.errorbar(x = protons_bins[:-1], y = protons_frac*100, label = "Protons", yerr = protons_err*100)
 plt.errorbar(x = type0_bins[:-1], y = type0_frac*100, label = "type 0", yerr = type0_err*100)
 
-plt.legend()
+
+
+plt.legend(loc = "upper center")
 plt.xlabel("P (GeV)")
 plt.ylabel("Percent Mislabeled")
 plt.title("Misidentified Particles in QCD Simulation data")
--- misIDcheck.py
+++ misIDcheck.py
@@ -1,6 +1,8 @@
+import uproot
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
+from scipy.optimize import curve_fit
 
 c = 3*10**8
 pionMass = 0.13957
@@ -13,24 +15,66 @@ kaonbfrac = 0.6356
 def f(L, p, mass, tau):
   return np.exp(-(L*mass)/(p*c*tau))
 
-def fintergral(p, mass, tau, bfrac):
-  return bfrac*integrate.quad(f, 0, 5, args = (p, mass, tau))[0]/integrate.quad(f, 0, np.inf, args = (p, mass, tau))[0]
+def fintergral(p, mass, tau, bfrac, length):
+  return 100*bfrac*integrate.quad(f, 0, length, args = (p, mass, tau))[0]/integrate.quad(f, 0, np.inf, args = (p, mass, tau))[0]
 
-def makeArray(p, mass, tau, bfrac):
+def makeArray(p, mass, tau, bfrac, length):
   arr = np.array([])
   for i in p:
-    arr = np.append(arr, fintergral(i, mass, tau, bfrac))
+    arr = np.append(arr, fintergral(i, mass, tau, bfrac, length))
   return arr
 
-#Plotting graph for Pion
-p = np.linspace(1, 100, 1000)
-plt.plot(p, makeArray(p, pionMass, pionTau, pionbfrac), label = "Pion")
+#Plotting graph for Pion, using length = 5
+pRange = np.linspace(20, 100, 1000)
+plt.plot(pRange, makeArray(pRange, pionMass, pionTau, pionbfrac, 5), label = "Pion")
 plt.xlabel("p (GeV)")
 plt.ylabel("f(p)")
 plt.title("Theoretical misID for pions and kaons")
 
-#plotting for Kaon
-plt.plot(p, makeArray(p, kaonMass, kaonTau, kaonbfrac), label = "Kaon")
+#Plotting graph for Kaon, using length = 5
+plt.plot(pRange, makeArray(pRange, kaonMass, kaonTau, kaonbfrac, 5), label = "Kaon")
 plt.legend()
 
 plt.savefig("img/misIDcheck.png")
+plt.close()
+
+#Fit the QCD simulation data to the theoretical pion and kaon plots (above) to give the length
+with uproot.open("/tmp/13TeV_2017_29r2_Up_QcdBgdPt18GeV_Sim09k.root") as file:
+  PT = file["WpNoMuID/DecayTree/mu_PT"].array(library = "np")/1000
+  isMuon = file["WpNoMuID/DecayTree/mu_ISMUON"].array(library = "np")
+  trueID = file["WpNoMuID/DecayTree/mu_TRUEID"].array(library = "np")
+  ETA = file["WpNoMuID/DecayTree/mu_ETA"].array(library = "np")
+
+#code taken from misID.py
+Ptot = PT[PT > 20]*np.cosh(ETA[PT > 20])
+trueID = trueID[PT > 20]
+isMuon = isMuon[PT > 20]
+kaons_P = Ptot[(trueID == 321)]
+pions_P = Ptot[(trueID == 211)]
+kaons_misID = Ptot[(isMuon == True) & (trueID == 321)]
+pions_misID = Ptot[(isMuon == True) & (trueID == 211)]
+kaons_hist, kaons_bins = np.histogram(kaons_P, bins = 50, range = [55, 200])
+pions_hist, pions_bins = np.histogram(pions_P, bins = 50, range = [55, 200])
+kaons_frac = np.histogram(kaons_misID, bins = 50, range = [55, 200])[0]/kaons_hist
+pions_frac = np.histogram(pions_misID, bins = 50, range = [55, 200])[0]/pions_hist
+kaons_err = np.sqrt((kaons_frac*(1 - kaons_frac))/kaons_hist)
+pions_err = np.sqrt((pions_frac*(1 - pions_frac))/pions_hist)
+
+plt.errorbar(x = kaons_bins[:-1], y = kaons_frac*100, label = "Kaon Data", yerr = kaons_err*100)
+plt.errorbar(x = pions_bins[:-1], y = pions_frac*100, label = "Pion Data", yerr = pions_err*100)
+plt.xlabel("P (GeV)")
+plt.ylabel("Percent Mislabeled")
+plt.title("Fitting QCD simulation misID data to get length")
+##########################
+
+kaonParams, kaonCov = curve_fit(lambda p, length: makeArray(p, kaonMass, kaonTau, kaonbfrac, length), kaons_bins[:-1], kaons_frac*100)
+pionParams, pionCov = curve_fit(lambda p, length: makeArray(p, pionMass, pionTau, pionbfrac, length), pions_bins[:-1], pions_frac*100)
+
+print(kaonParams[0], np.sqrt(np.diag(kaonCov)))
+print(pionParams[0], np.sqrt(np.diag(pionCov)))
+
+pRange = np.linspace(55, 200, 1000)
+plt.plot(pRange, makeArray(pRange, kaonMass, kaonTau, kaonbfrac, kaonParams[0]), label = "Kaon Fit")
+plt.plot(pRange, makeArray(pRange, pionMass, pionTau, pionbfrac, pionParams[0]), label = "Pion Fit")
+plt.legend()
+plt.savefig("img/misIDfit.png")
